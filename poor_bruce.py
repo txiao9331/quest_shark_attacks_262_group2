f5fb5b2437dd04d1fcc8a5f5700f307c3ba565dd
txiao9331 <541891213@qq.com>
2024-03-07 17:15:05 +0100

    0307
    Co-authored-by: krantagat <krantagat@users.noreply.github.com>
    Co-authored-by: maossand <maossand@users.noreply.github.com>
    Co-authored-by: torras.nuria@gmail.com <torras.nuria@gmail.com>

--- poor_bruce.py
+++ poor_bruce.py
@@ -9,15 +9,39 @@ def drop_null_values(df):
     df = df.dropna(subset=['date'])
     return df
 
-# format date, year (into int), and time column 
+# format column year (into int)
+def format_year(df):
+    df[df['year'].isnull()]  # display all rows with null values
+    df.loc[797] = df.loc[797].fillna(value=2017)  # Replace NaN in the specified row with value
+    df[['year']]=df[['year']].fillna(0).astype(int)
+    return df
+
+# filter_df
+def filter_year(df):
+    # .copy() to create a new DataFrame
+    filtered_df=df.copy()
+    # filter years before 2010
+    filtered_df = filtered_df.loc[filtered_df['year'] > 2009]
+    return filtered_df
+
+# format column date
+def format_date(filtered_df):
+    filtered_df['date']=filtered_df['date'].str.replace(" ","-").str.replace("-","")
+    # drop all rows where date contains string "Reported"
+    filtered_df = filtered_df[~filtered_df['date'].str.contains("Reported")]
+    return filtered_df
+
 
 # in columns country ---Martino
 
 # in columns year & date -->season Xiaobo
 
 
-# filter the years after 2010 -->Nuria
-
+#filter all years before 2010  -->Nuria
+# #Use strip to remove the uncentered data (observations 325,324,84 are uncentered, leaving a blank space)
+# year_filtered_df = df.loc[df['year'] > '2009'].strip()
+# #drop the observation 799, blank space
+# year_filtered_df = year_filtered_df.drop(index[799])
 
 
 # filter time and group them to  ---> Kranta
@@ -28,7 +52,7 @@ def drop_null_values(df):
 
 
 
-# filter the age by group and replace --->??
+# filter the age by group and replace --->Stéphanie
 
 # main function
 
@@ -42,3 +66,12 @@ def drop_null_values(df):
 
 
 # specie [MAYBE]
+
+
+# def main():
+#     print("Main function")
+#     function1()
+#     function2()
+
+# if __name__ == "__main__":
+#     main()
